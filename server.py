dd43b129e96a0165f93f61aaec764362d9819248
StevenRagav <55227679+StevenRagav@users.noreply.github.com>
2023-08-18 21:37:54 +0800

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "Welcome"
-    message =  "Hello World, " + name + "!\n"
+    message =  "Hello World How are you?, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
